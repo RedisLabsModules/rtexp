660ba313ad378afbd6683e9c9001ab7b647b2112
daTokenizer <adam.inf@gmail.com>
2017-11-05 12:10:24 +0200

    start building c tests for core functionallity

--- tests/store_test.c
+++ tests/store_test.c
@@ -1,33 +1,18 @@
-#ifndef RTX_STORE_H
-#define RTX_STORE_H
+/* This is a stand-alone implementation of a real-time expiration data store.
+ * It is basically a min heap <key, exp_version> sorted by expiration,
+ * with a map of [key] -> <exp_version, exp> on the side
+ */
+#include "src/rtexp_store.h"
 
-#include "trie/triemap.h"
-#include "util/heap.h"
-#include "util/millisecond_time.h"
+#include "src/util/millisecond_time.h"
 
-#define RTXS_OK 0
-#define RTXS_ERR 1
-
-/***************************
- *        STRUCTS
- ***************************/
-
-typedef struct rtxs_node {
-  char* key;
-  mtime_t expiration;
-  int version;
-} RTXElementNode;
-
-typedef struct rtxs_store {
-  heap_t* sorted_keys;        // [exp] -> <key, exp_version>
-  TrieMap* element_node_map;  // [key] -> <exp_version, exp_timestamp>
-} RTXStore;
+#include <time.h>
 
 /***************************
- *     CONSTRUCTOR
+ *     CONSTRUCTORS
  ***************************/
+
 RTXStore* newRTXStore(void);
-
 /************************************
  *   General DS handling functions
  ************************************/
@@ -36,37 +21,49 @@ RTXStore* newRTXStore(void);
  * Insert expiration for a new key or update an existing one
  * @return RTXS_OK on success, RTXS_ERR on error
  */
-int set_element_exp(RTXStore* store, char* key, mtime_t ttl_ms);
+// int set_element_exp(RTXStore* store, char* key, mtime_t ttl_ms);
 
 /*
  * Get the expiration value for the given key
  * @return datetime of expiration (in milliseconds) on success, -1 on error
  */
-mtime_t get_element_exp(RTXStore* store, char* key);
+// mtime_t get_element_exp(RTXStore* store, char* key);
 
 /*
  * Remove expiration from the data store for the given key
  * @return RTXS_OK
  */
-int del_element_exp(RTXStore* store, char* key);
+// int del_element_exp(RTXStore* store, char* key);
 
 /*
  * @return the closest element expiration datetime (in milliseconds), or -1 if DS is empty
  */
-mtime_t next_at(RTXStore* store);
+// mtime_t next_at(RTXStore* store);
 
 /*
  * Remove the element with the closest expiration datetime from the data store and return it's key
  * @return the key of the element with closest expiration datetime
  */
 // TODO: there might be a deletion issue here (where is the key string stored?)
-char* pull_next(RTXStore* store);
+// char* pull_next(RTXStore* store);
 
 /*
  * Wait Remove the element with the closest expiration datetime from the data store and return it's
  * key
  * @return the key of the element with closest expiration datetime
  */
-char* wait_and_pull(RTXStore* store);
+// char* wait_and_pull(RTXStore* store);
 
-#endif
+int test_set_element_exp() {
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+  test_funcs = [
+
+  ];
+  for (i = 1; i <= sizeof(test_funcs); i--) {
+    if (test_funcs[(i - 1)]() != 0) return i;
+  }
+  return 0;
+}
