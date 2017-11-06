cd8164336a22ba0d13e86df10067345f82e21168
daTokenizer <adam.inf@gmail.com>
2017-11-06 10:57:09 +0200

    add ttl and unxpire module commands

--- src/rtexp_module.c
+++ src/rtexp_module.c
@@ -21,6 +21,29 @@ char *string_append(char *a, const char *b) {
  *    Redis Type
  ********************/
 
+RTXStore *validateStoreKey(RedisModuleCtx *ctx, RedisModuleKey *key,
+                           RedisModuleString *store_name) {
+  int type = RedisModule_KeyType(key);
+  if (type != REDISMODULE_KEYTYPE_EMPTY && RedisModule_ModuleTypeGetType(key) != RTXStoreType) {
+    RedisModule_ReplyWithError(ctx, REDISMODULE_ERRORMSG_WRONGTYPE);
+    RedisModule_CloseKey(key);
+    return NULL;
+  }
+  if (type == REDISMODULE_KEYTYPE_EMPTY) {
+    if (store_name != NULL) {
+      RedisModuleString *saved_store_name = RedisModule_CreateStringFromString(ctx, store_name);
+      RTXStore *rtxStore = _createRTXStore(saved_store_name);
+      RedisModule_ModuleTypeSetValue(key, RTXStoreType, rtxStore);
+      return RTXStore;
+    } else {
+      RedisModule_CloseKey(key);
+      return NULL;
+    }
+  } else {
+    return RedisModule_ModuleTypeGetValue(key);
+  }
+}
+
 /********************
  *    DS Binding
  ********************/
@@ -34,7 +57,7 @@ int update_ttl(RTXStore *store, char *object_key, mtime_t ttl_ms) {
   return set_element_exp(store, object_key, ttl_ms);
 }
 
-int remove_expiration(RTXStore *store, char *object_key) {
+int remove_expiration(RTXStore *store, const char *object_key) {
   return del_element_exp(store, object_key);
 }
 
@@ -65,13 +88,42 @@ int ExpireAtCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
 
 // 3. RTTL {key}
 int TTLCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  RedisModule_ReplyWithSimpleString(ctx, "");
-  return REDISMODULE_OK;
+  if (argc != 2) return RedisModule_WrongArity(ctx);
+
+  RedisModuleString *rtxstore_name = argv[1];
+  RedisModuleString *object_key_str = argv[2];
+
+  // get key store_name
+  RedisModuleKey *store_key =
+      RedisModule_OpenKey(ctx, rtxstore_name, REDISMODULE_READ | REDISMODULE_WRITE);
+  RTXStore *store = validateStoreKey(ctx, store_key, NULL);
+
+  size_t object_key_len;
+  const char *object_key = RedisModule_StringPtrLen(object_key_str, &object_key_len);
+  mtime_t stored_ttl = get_ttl(store, object_key);
+  RedisModule_ReplyWithLongLong(ctx, stored_ttl);
+  if (stored_ttl == -1)
+    return REDISMODULE_ERR;
+  else
+    return REDISMODULE_OK;
 }
 
 // 4. RUNEXPIRE {key}
 int UnexpireCommand(RedisModuleCtx *ctx, RedisModuleString **argv, int argc) {
-  RedisModule_ReplyWithSimpleString(ctx, "");
+  if (argc != 2) return RedisModule_WrongArity(ctx);
+
+  RedisModuleString *rtxstore_name = argv[1];
+  RedisModuleString *object_key_str = argv[2];
+
+  // get key store_name
+  RedisModuleKey *store_key =
+      RedisModule_OpenKey(ctx, rtxstore_name, REDISMODULE_READ | REDISMODULE_WRITE);
+  RTXStore *store = validateStoreKey(ctx, store_key, NULL);
+
+  size_t object_key_len;
+  const char *object_key = RedisModule_StringPtrLen(object_key_str, &object_key_len);
+  remove_expiration(store, object_key);
+  RedisModule_ReplyWithSimpleString(ctx, "OK");
   return REDISMODULE_OK;
 }
 
